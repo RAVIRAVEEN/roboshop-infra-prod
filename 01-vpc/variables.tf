9a1b5aa2bf6c053e2bbc31be7e399535e840eaf1
localstar1996 <localstar1996@gmail.com>
2024-07-18 16:01:23 +0530

    first commit

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -1,5 +1,5 @@
 variable "cidr_block" {
-  default = "10.1.0.0/16"
+  default = "10.1.1.0/16"
   }
 
 variable "common_tags" {
@@ -33,19 +33,19 @@ variable "public_subnet_tags" {
 }
 
 variable "public_subnet_cidr" {
-    default = ["10.5.1.0/24", "10.5.2.0/24"]
+    default = ["10.2.1.0/24", "1.2.2.0/24"]
 
   
 }
 
 variable "private_subnet_cidr" {
-    default = ["10.5.11.0/24", "10.5.12.0/24"]
+    default = ["10.2.11.0/24", "10.2.12.0/24"]
 
   
 }
 
 variable "database_subnet_cidr" {
-    default = ["10.5.21.0/24", "10.5.22.0/24"]
+    default = ["10.2.21.0/24", "10.2.22.0/24"]
 
 }
 
